fffeb38e00a6678d5a4d363ee51aaa165a5bae01
박현성 <parkhyunsung@baghyeonseong-ui-MacBookPro.local>
2024-09-19 18:27:40 +0900

    리뷰클릭 -develop 라이브러리설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './global.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RecoilRoot } from 'recoil'
+
+// QueryClient 인스턴스 생성
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </RecoilRoot>
+
+
 )
