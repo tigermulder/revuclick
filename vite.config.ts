2b5eae70c49a2999669df0e318fb714e85ab1e7f
박현성 <parkhyunsung@baghyeonseong-ui-MacBookPro.local>
2024-10-10 10:10:33 +0900

    리뷰클릭 production 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import tsconfigPaths from "vite-tsconfig-paths"
 import svgr from "vite-plugin-svgr"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -9,20 +10,35 @@ export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
   return {
-    base: "./", // 기본 경로 설정
+    base: "/", // 기본 경로 설정
     plugins: [react(), tsconfigPaths(), svgr()], // Vite 플러그인
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"), // 'src' 폴더를 '@'로 매핑
+        pages: path.resolve(__dirname, "./src/pages"), // 'pages' 폴더에 별칭 매핑
+        components: path.resolve(__dirname, "./src/components"), // 'components' 매핑
+        assets: path.resolve(__dirname, "./src/assets"), // 'assets' 매핑
+        store: path.resolve(__dirname, "./src/store"), // store 경로 별칭
+        utils: path.resolve(__dirname, "./src/utils"), // utils 경로 별칭
+        types: path.resolve(__dirname, "./src/types"), // types 경로 별칭
+        services: path.resolve(__dirname, "./src/services"), // services 경로 별칭
+        hooks: path.resolve(__dirname, "./src/hooks"), // hooks 경로 별칭
+      },
+    },
     server: {
       proxy: {
         "/api": {
           target: process.env.VITE_SERVER_URL,
           changeOrigin: true, // 서버의 Origin을 프록시 서버의 Origin으로 변경
-          secure: false, // HTTPS를 사용할 때 인증서 검증을 무시 (개발 환경에서 유용)
+          secure: false, // HTTPS를 사용할 때 인증서 검증을 무시 (개발 환경)
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
       },
     },
     build: {
-      outDir: "dist", // 빌드 출력 디렉토리 확인
+      outDir: "dist",
+      emptyOutDir: true, // 빌드 시 dist 폴더를 비움
+      sourcemap: false,
       rollupOptions: {
         output: {
           assetFileNames: (assetInfo) => {
@@ -36,6 +52,7 @@ export default defineConfig(({ mode }) => {
           entryFileNames: "assets/js/[name]-[hash].js",
         },
       },
+      assetsInlineLimit: 4096, // 기본값(4KB)
     },
   }
 })
